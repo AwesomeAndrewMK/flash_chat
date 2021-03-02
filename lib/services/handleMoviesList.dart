e16aa24981c7298202ad367f12edf75976bd50d0
Andrii <awesomeakm@gmail.com>
2021-03-02 09:17:55 +0200

    Added additional method to work with dates

--- lib/services/handleMoviesList.dart
+++ lib/services/handleMoviesList.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:collection/collection.dart';
 import 'package:intl/intl.dart';
@@ -17,6 +18,12 @@ class HandleMoviesList {
   int _findDiffInDays(String date) {
     DateTime now = DateTime.now();
     return now.difference(DateTime.parse(date)).inDays;
+  }
+
+  // can use it in case you want release date >= now
+  bool isBiggerThanNow(String date) {
+    return Timestamp.fromDate(DateTime.parse(date)).millisecondsSinceEpoch >=
+        Timestamp.fromDate(DateTime.now()).millisecondsSinceEpoch;
   }
 
   Map<String, List<dynamic>> getGroupedMovies() {
